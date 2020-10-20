45102d530eb94567f717c1541d183df4789b22c2
Darshan Singh <darshan.singh.tech@gmail.com>
2020-10-20 09:15:05 -0400

    Create installer.py

---
+++ installer.py
@@ -0,0 +1,18 @@
+# import os
+
+# List out various programs to install:
+# Steam
+# Vim (?)
+
+
+# Source: https://stackabuse.com/executing-shell-commands-with-python/
+
+
+# home_dir = os.system("cd")
+# print("`cd ~` ran with exit code %d" % home_dir)
+
+
+# import subprocess
+
+# list_files = subprocess.run(["cd"])
+# print("The exit code was: %d" % list_files.returncode)
