51b1be694466fbb6585b3414fbd2af90f570b9c0
Darshan Singh <darshan.singh.tech@gmail.com>
2020-11-01 11:46:30 -0500

    Updated README, Updated PITA Applications list

--- installer.py
+++ installer.py
@@ -21,13 +21,16 @@ while True:
         print("Not a valid package manager, sorry")
         packageManager = input('Enter your package manager: ')
 
-
+# ------------------------------------------------------------------
 # Sources used:
 # Source: https://stackoverflow.com/questions/25735389/storing-items-in-a-list-for-python
 # Source: https://stackabuse.com/executing-shell-commands-with-python/
 # Potentially useful: https://unix.stackexchange.com/questions/71136/how-can-i-see-which-package-manager-is-running
 
+# ------------------------------------------------------------------
 # PITA Install Applications
 # Source (VS Code): https://duckduckgo.com/?t=ffab&q=install+vscode+from+terminal&ia=web
 # Source (Zoom): https://support.zoom.us/hc/en-us/articles/204206269-Installing-or-updating-Zoom-on-Linux#h_adcc0b66-b2f4-468b-bc7a-12c182f354b7
 # Source (LibreOffice): https://wiki.documentfoundation.org/Documentation/Install/Linux#Terminal-Based_Install
+# Source (HandBrake): https://itsfoss.com/install-handbrake-ubuntu/
+# Source (HandBrake - Other OS's): https://handbrake.fr/docs/en/latest/developer/build-linux.html
