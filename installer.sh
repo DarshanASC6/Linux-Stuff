2a900edaac0da8130b47e8c2af109c7e3307487a
Darshan Singh <darshan.singh.tech@gmail.com>
2020-12-07 11:37:27 -0700

    Added in installer.sh file
    Learned (sorta) how to write bash scripts. I basically just copy pasted my Python code in and translated it. Also it only supports apt-get at the moment cause I was lazy.

---
+++ installer.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sudo apt-get update
+
+sudo apt-get upgrade
+
+sudo apt-get install steam lutris vim discord gimp git firefox libreoffice vlc qbittorrent python3
+
+: 'Sources:
+https://www.linuxquestions.org/questions/programming-9/bash-determine-what-package-manager-to-use-817036/
+https://askubuntu.com/questions/175836/creating-a-script-to-install-software-after-a-clean-install
+https://linuxhint.com/30_bash_script_examples/#t1 '
+
+echo "Hello World"
