6bf600498f4a3e0f3c492d811d809e92347c4bcb
Darshan Singh <darshnyc@gmail.com>
2020-12-29 23:46:27 -0500

    Updated setup script to be more organized

--- installer.sh
+++ installer.sh
@@ -2,13 +2,39 @@
 
 sudo apt-get update
 
-sudo apt-get upgrade
+#Steam
+sudo apt-get install steam 
 
-sudo apt-get install steam lutris vim discord gimp git firefox libreoffice vlc qbittorrent python3
+#Lutris
+sudo apt-get install lutris 
 
-: 'Sources:
+#Vim
+sudo apt-get install vim 
+
+#Discord
+sudo apt-get install discord 
+
+#Gimp
+sudo apt-get install gimp 
+
+#Git
+sudo apt-get install git 
+
+#FireFox
+sudo apt-get install firefox 
+
+#LibreOffice
+sudo apt-get install libreoffice 
+
+#VLC Media Player
+sudo apt-get install vlc 
+
+#Puthon 3
+sudo apt-get install python3
+
+echo "Finished installing all software"
+
+'Sources:
 https://www.linuxquestions.org/questions/programming-9/bash-determine-what-package-manager-to-use-817036/
 https://askubuntu.com/questions/175836/creating-a-script-to-install-software-after-a-clean-install
 https://linuxhint.com/30_bash_script_examples/#t1 '
-
-echo "Hello World"
