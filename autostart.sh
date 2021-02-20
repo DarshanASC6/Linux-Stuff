bc09982bd1f155404836b62df024f12b24d1261e
Darshan Singh <darshnyc@gmail.com>
2021-02-19 23:45:52 -0500

    Cleaned up status bar script, added in icons

--- autostart.sh
+++ autostart.sh
@@ -3,8 +3,30 @@
 # Set wallpaper for dwm
 xwallpaper --zoom ~/Pictures/Wallpapers/wallpaper.jpg
 
+pwr () {
+ 	icon=""
+	capacity=$(echo $(cat /sys/class/power_supply/BAT0/capacity)"%")
+	printf "%s %s" "$icon" "$capacity"
+}
+
+dte () {
+	icon=""
+	date="$(date '+%a %b %Y')"
+	hour_minute="$(date '+%I:%M ')"
+	printf "%s %s" "$icon" "$hour_minute" "$date"
+}
+
+volume () {
+	icon=""
+	volume=$(awk -F"[][]" '/Left:/ { print $2}' <(amixer sget Master))
+	printf "%s %s" "$icon" "$volume"
+}
+
 # DWM Status bar config
 while true; do
-	xsetroot -name "$(echo "Volume: $(awk -F"[][]" '/Left:/ { print $2}' <(amixer sget Master))") | $(date "+%a %b %I:%M %Y") | $(echo "Battery: $(cat /sys/class/power_supply/BAT0/capacity)%")" 
+	xsetroot -name "$(volume) | $(dte) | $(pwr)" 
 	sleep 1s
 done &
+
+# Enable tapping on the trackpad
+xinput set-prop 12 329 1
